0ef0a1f6cd7c2fd0f755da7e2dad5071adac4d10
yuezongzhe <yuezongzhe@oradt.com>
2019-05-31 13:46:04 +0800

    shape 的使用。 博客已经更新。。

--- recyclerviewdemo/src/main/java/example/yzz/recyclerviewdemo/toasttest/ToastActivity.java
+++ recyclerviewdemo/src/main/java/example/yzz/recyclerviewdemo/toasttest/ToastActivity.java
@@ -1,6 +1,7 @@
 package example.yzz.recyclerviewdemo.toasttest;
 
 import android.content.DialogInterface;
+import android.graphics.Paint;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,10 +16,45 @@ import example.yzz.recyclerviewdemo.R;
 
 public class ToastActivity extends AppCompatActivity {
 
+    private TextView tv_text;
+    private TextView tv_text1;
+    private TextView tv_text2;
+    private TextView tv_text3;
+    private TextView tv_text4;
+    private TextView tv_text5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+
+
+        tv_text = findViewById(R.id.tv_text);
+        tv_text1 = findViewById(R.id.tv_text1);
+        tv_text2 = findViewById(R.id.tv_text2);
+        tv_text3 = findViewById(R.id.tv_text3);
+        tv_text4 = findViewById(R.id.tv_text4);
+        tv_text5 = findViewById(R.id.tv_text5);
+
+
+
+        //下划线,两种实现
+        tv_text.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        tv_text1.getPaint().setUnderlineText(true);
+
+        //抗锯齿
+        tv_text2.getPaint().setAntiAlias(true);
+
+        //中划线
+        tv_text3.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+
+        // 设置中划线并加清晰 
+        tv_text4.getPaint().setFlags(Paint. STRIKE_THRU_TEXT_FLAG|Paint.ANTI_ALIAS_FLAG);
+
+        //字体加粗
+        tv_text5.getPaint().setFakeBoldText(true);
+
+
     }
 
     public void btn_1(View v) {
@@ -32,6 +68,8 @@ public class ToastActivity extends AppCompatActivity {
                 dialog.cancel();
             }
         });
+
+
     }
 
     public void btn_2(View v) {
