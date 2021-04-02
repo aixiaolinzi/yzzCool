abb9a6ad5ef8731368068b3a080b1b75df5741a1
YueZongzhe <yuezz0926@thundersoft.com>
2021-04-02 14:47:47 +0800

    字节流动 文章开始

---
+++ app/src/main/java/com/yue/openGLC/OpenGLESMainActivity.java
@@ -0,0 +1,33 @@
+package com.yue.openGLC;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.yue.openGLC.gles1.OpenGLES1Activity;
+import com.yue.yueapp.R;
+
+
+
+/**
+ *Time: 2021/4/2
+ *Author:yzzCool
+ *Description:
+ */
+public class OpenGLESMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_open_gl_es);
+    }
+
+    public void flow1(View view) {
+        //NDK OpenGL ES 3.0 开发（一）：绘制一个三角形
+        startActivity(new Intent(this, OpenGLES1Activity.class));
+    }
+
+}
