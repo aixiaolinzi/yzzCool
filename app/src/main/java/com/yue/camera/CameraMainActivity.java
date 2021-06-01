cc16f58172def2ae1346f68b59d0c03d4b3576cd
YueZongzhe <yuezz0926@thundersoft.com>
2021-06-01 20:16:51 +0800

    Android Camera mode 首次初始化。

---
+++ app/src/main/java/com/yue/camera/CameraMainActivity.java
@@ -0,0 +1,16 @@
+package com.yue.camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yue.yueapp.R;
+
+public class CameraMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_main);
+    }
+}
---
+++ app/src/main/java/com/yue/camera/process1/CameraBase1Activity.java
@@ -0,0 +1,22 @@
+package com.yue.camera.process1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yue.yueapp.R;
+
+
+/**
+ *Time: 2021/6/1
+ *Author:yzzCool
+ *Description: camera preview，有预览
+ */
+public class CameraBase1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_base1);
+    }
+}
