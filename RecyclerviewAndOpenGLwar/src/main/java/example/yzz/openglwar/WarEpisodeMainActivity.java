6aba0e057443f8d16a51083232abfedcf617057f
YueZongzhe <yuezz0926@thundersoft.com>
2021-09-13 15:44:11 +0800

    Android多媒体之GL-ES战记第一集--勇者集结
    开始我们的勇者之旅。

---
+++ RecyclerviewAndOpenGLwar/src/main/java/example/yzz/openglwar/WarEpisodeMainActivity.java
@@ -0,0 +1,24 @@
+package example.yzz.openglwar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import example.yzz.recyclerviewdemo.R;
+
+public class WarEpisodeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_war_episode_main);
+    }
+
+    public void onClick1(View view) {
+
+    }
+
+    public void onClick2(View view) {
+    }
+}
